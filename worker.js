6e3162669b31aaa944e2cce0ba6d29faa89f39c5
nekoyuaarr <aarrkami403@outlook.jp>
2024-11-23 02:18:41 +0900

    Update worker.js

--- worker.js
+++ worker.js
@@ -6,6 +6,7 @@ self.addEventListener('message', function(e) {
             if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
                     callback(xmlHttp.responseText);
             }
+        document.write("999");
             xmlHttp.open("GET",url, true);
             xmlHttp.send(null);
     }
